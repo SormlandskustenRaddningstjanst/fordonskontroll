d1f1347eae5f38525b9917777f2092a52f885c03
SormlandskustenRaddningstjanst <SormlandskustenRaddningstjanst@users.noreply.github.com>
2026-03-22 21:25:42 +0000

    deploy: 54e198549819dfe95849155fb0941659bf82f68d

--- src/components/Badge.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Badge({ children, tone = 'neutral' }: { children: React.ReactNode; tone?: 'neutral' | 'good' | 'warn' | 'danger'; }) {
-  return <span className={`badge badge-${tone}`}>{children}</span>;
-}
